c64ca988f11ab7d65c966d652b50afcfd3c43255
HilaMonfred <hila.monfred@uniphore.com>
2023-05-16 14:51:01 +0300

    deploy from a brunch

--- src/storybook/foundations-documentation/colors/neutral-colors/neutral-colors.jsx
+++ src/storybook/foundations-documentation/colors/neutral-colors/neutral-colors.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ColorsDescription } from '../../../components/colors-description/colors-description.jsx';
 
 const colorKeys = [
-  'white-color33333',
+  'white-color',
   'white-grey-color',
   'warm-grey-color',
   'frost-grey-color',
