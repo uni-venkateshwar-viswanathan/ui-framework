4a795e26d614722b758ef85131f8a7e4bf1289a2
Nastia Klitsuk <nkiltsuk@jacada.com>
2023-05-16 11:13:21 +0300

    https://uniphore.atlassian.net/browse/CD-5607-added uniphore logo image

--- .storybook/uniphore-theme.js
+++ .storybook/uniphore-theme.js
@@ -1,10 +1,11 @@
 import { create } from '@storybook/theming/create';
 import uniphoreLogo from './uniphore-logo.png';
 
+
 export default create({
   base: 'light',
 
-  brandTitle: '<img src="./uniphore-logo.svg" width="10px" height="10px"/>',
+  brandTitle: 'Uniphore Design System',
   brandUrl: 'https://style.uniphore.com',
   brandImage: uniphoreLogo,
   brandTarget: '_self',
