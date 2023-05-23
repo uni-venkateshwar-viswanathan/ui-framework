c0f6eaead0df5a2b21e35f77b5b74a6437cb8d5b
HilaMonfred <hila.monfred@uniphore.com>
2023-05-23 14:25:21 +0300

    https://uniphore.atlassian.net/browse/CD-5886 - fix tests

--- src/assets/icons/icomoon/style.js
+++ src/assets/icons/icomoon/style.js
@@ -31,107 +31,81 @@ export const uniphoreIconMoonStyle = css`
 
   .icon-video:before {
     content: '\\e967';
-    color: #6d6f79;
   }
   .icon-video-library:before {
     content: '\\e968';
-    color: #6d6f79;
   }
   .icon-visibility:before {
     content: '\\e969';
-    color: #6d6f79;
   }
   .icon-visibility-off:before {
     content: '\\e96a';
-    color: #6d6f79;
   }
   .icon-zap:before {
     content: '\\e96b';
-    color: #6d6f79;
   }
   .icon-zoom-in:before {
     content: '\\e96c';
-    color: #6d6f79;
   }
   .icon-zoom-out:before {
     content: '\\e96d';
-    color: #6d6f79;
   }
   .icon-account-circle:before {
     content: '\\e900';
-    color: #6d6f79;
   }
   .icon-add:before {
     content: '\\e90f';
-    color: #6d6f79;
   }
   .icon-add-call:before {
     content: '\\e910';
-    color: #6d6f79;
   }
   .icon-add-chart:before {
     content: '\\e911';
-    color: #6d6f79;
   }
   .icon-add-circle-outline:before {
     content: '\\e912';
-    color: #6d6f79;
   }
   .icon-alternate-email:before {
     content: '\\e913';
-    color: #6d6f79;
   }
   .icon-announcements:before {
     content: '\\e914';
-    color: #6d6f79;
   }
   .icon-arrow-down-circle:before {
     content: '\\e915';
-    color: #6d6f79;
   }
   .icon-arrow-downward:before {
     content: '\\e916';
-    color: #6d6f79;
   }
   .icon-arrow-up-circle:before {
     content: '\\e917';
-    color: #6d6f79;
   }
   .icon-arrow-upward:before {
     content: '\\e918';
-    color: #6d6f79;
   }
   .icon-assignment:before {
     content: '\\e919';
-    color: #6d6f79;
   }
   .icon-assignment-late:before {
     content: '\\e91a';
-    color: #6d6f79;
   }
   .icon-auto-renew:before {
     content: '\\e91b';
-    color: #6d6f79;
   }
   .icon-avatar-placeholder:before {
     content: '\\e91c';
-    color: #6d6f79;
   }
   .icon-bolt-circle:before {
     content: '\\e91d';
-    color: #6d6f79;
   }
   .icon-bookmark-border:before {
     content: '\\e91e';
-    color: #6d6f79;
   }
   .icon-check-circle:before {
     content: '\\e91f';
-    color: #6d6f79;
   }
   .icon-check-circle-solid:before {
     content: '\\e920';
-    color: #6d6f79;
   }
   .icon-clipboard:before {
     content: '\\e921';
@@ -153,259 +127,195 @@ export const uniphoreIconMoonStyle = css`
   }
   .icon-delete:before {
     content: '\\e927';
-    color: #6d6f79;
   }
   .icon-detect:before {
     content: '\\e928';
-    color: #6d6f79;
   }
   .icon-download:before {
     content: '\\e929';
-    color: #6d6f79;
   }
   .icon-drop-down-arrow:before {
     content: '\\e92a';
-    color: #6d6f79;
   }
   .icon-drop-down-arrow-collapse:before {
     content: '\\e92b';
-    color: #6d6f79;
   }
   .icon-edit:before {
     content: '\\e92c';
-    color: #6d6f79;
   }
   .icon-edit-square:before {
     content: '\\e92d';
-    color: #6d6f79;
   }
   .icon-email:before {
     content: '\\e92e';
-    color: #6d6f79;
   }
   .icon-emergency:before {
     content: '\\e92f';
-    color: #6d6f79;
   }
   .icon-entity:before {
     content: '\\e930';
-    color: #6d6f79;
   }
   .icon-expand:before {
     content: '\\e931';
-    color: #6d6f79;
   }
   .icon-extract:before {
     content: '\\e932';
-    color: #6d6f79;
   }
   .icon-favorite-border:before {
     content: '\\e933';
-    color: #6d6f79;
   }
   .icon-full-screen:before {
     content: '\\e934';
-    color: #6d6f79;
   }
   .icon-grid-view:before {
     content: '\\e935';
-    color: #6d6f79;
   }
   .icon-group:before {
     content: '\\e936';
-    color: #6d6f79;
   }
   .icon-history:before {
     content: '\\e937';
-    color: #6d6f79;
   }
   .icon-home:before {
     content: '\\e938';
-    color: #6d6f79;
   }
   .icon-image-solid:before {
     content: '\\e939';
-    color: #6d6f79;
   }
   .icon-info:before {
     content: '\\e93a';
-    color: #6d6f79;
   }
   .icon-inside-organization:before {
     content: '\\e93b';
-    color: #6d6f79;
   }
   .icon-intent:before {
     content: '\\e93c';
-    color: #6d6f79;
   }
   .icon-keyboard:before {
     content: '\\e93d';
-    color: #6d6f79;
   }
   .icon-link:before {
     content: '\\e93e';
-    color: #6d6f79;
   }
   .icon-lock:before {
     content: '\\e93f';
-    color: #6d6f79;
   }
   .icon-login:before {
     content: '\\e940';
-    color: #6d6f79;
   }
   .icon-message:before {
     content: '\\e941';
-    color: #6d6f79;
   }
   .icon-money-circle:before {
     content: '\\e942';
-    color: #6d6f79;
   }
   .icon-more-horizontal:before {
     content: '\\e943';
-    color: #6d6f79;
   }
   .icon-more-vert:before {
     content: '\\e944';
-    color: #6d6f79;
   }
   .icon-movie:before {
     content: '\\e945';
-    color: #6d6f79;
   }
   .icon-navigate-before:before {
     content: '\\e946';
-    color: #6d6f79;
   }
   .icon-navigate-next:before {
     content: '\\e947';
-    color: #6d6f79;
   }
   .icon-notification:before {
     content: '\\e948';
-    color: #6d6f79;
   }
   .icon-notification-on:before {
     content: '\\e949';
-    color: #6d6f79;
   }
   .icon-outside-organization:before {
     content: '\\e94a';
-    color: #6d6f79;
   }
   .icon-perm-media:before {
     content: '\\e94b';
-    color: #6d6f79;
   }
   .icon-person-pin:before {
     content: '\\e94c';
-    color: #6d6f79;
   }
   .icon-phrase:before {
     content: '\\e94d';
-    color: #6d6f79;
   }
   .icon-preview:before {
     content: '\\e94e';
-    color: #6d6f79;
   }
   .icon-publish:before {
     content: '\\e94f';
-    color: #6d6f79;
   }
   .icon-push-pin:before {
     content: '\\e950';
-    color: #6d6f79;
   }
   .icon-remove:before {
     content: '\\e951';
-    color: #6d6f79;
   }
   .icon-remove-circle-outline:before {
     content: '\\e952';
-    color: #6d6f79;
   }
   .icon-rename:before {
     content: '\\e953';
-    color: #6d6f79;
   }
   .icon-report-problem:before {
     content: '\\e954';
-    color: #6d6f79;
   }
   .icon-save:before {
     content: '\\e955';
-    color: #6d6f79;
   }
   .icon-save-alt:before {
     content: '\\e956';
-    color: #6d6f79;
   }
   .icon-search1:before {
     content: '\\e957';
-    color: #6d6f79;
   }
   .icon-sentiment-dissatisfied:before {
     content: '\\e958';
-    color: #6d6f79;
   }
   .icon-sentiment-extremely-dissatisfied:before {
     content: '\\e959';
-    color: #6d6f79;
   }
   .icon-sentiment-neutral:before {
     content: '\\e95a';
-    color: #6d6f79;
   }
   .icon-sentiment-satisfied:before {
     content: '\\e95b';
-    color: #6d6f79;
   }
   .icon-sentiment-very-satisfied:before {
     content: '\\e95c';
-    color: #6d6f79;
   }
   .icon-settings1:before {
     content: '\\e95d';
-    color: #6d6f79;
   }
   .icon-settings-filled:before {
     content: '\\e95e';
-    color: #6d6f79;
   }
   .icon-share:before {
     content: '\\e95f';
-    color: #6d6f79;
   }
   .icon-shared-slide-solid:before {
     content: '\\e960';
-    color: #6d6f79;
   }
   .icon-star:before {
     content: '\\e961';
-    color: #6d6f79;
   }
   .icon-star-border:before {
     content: '\\e962';
-    color: #6d6f79;
   }
   .icon-star-half:before {
     content: '\\e963';
-    color: #6d6f79;
   }
   .icon-syncing-circle:before {
     content: '\\e964';
-    color: #6d6f79;
   }
   .icon-team:before {
     content: '\\e965';
-    color: #6d6f79;
   }
   .icon-user:before {
     content: '\\e966';
-    color: #6d6f79;
   }
   .icon-settings:before {
     content: '\\e901';
