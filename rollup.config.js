179196c076075dace72f65bb8797fead96efbd73
Nastia Klitsuk <nkiltsuk@jacada.com>
2023-06-13 13:23:48 +0300

    fixing Unresolved import error

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import css from "rollup-plugin-import-css";
 import babel from "@rollup/plugin-babel";
 import { copy } from '@web/rollup-plugin-copy';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 
 export default {
     input:'src/index.js',
@@ -9,6 +11,7 @@ export default {
         format: 'esm',
     },
     plugins: [
+        nodeResolve(),
         css({output: 'uniphore-styles.css'}),
         babel({exclude: 'node_modules/**' }),
         copy({
